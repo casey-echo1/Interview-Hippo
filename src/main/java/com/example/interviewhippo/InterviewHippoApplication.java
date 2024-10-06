e4c717948caefaef6beb853731b122d7f37a155d
Casey Echternacht <casey.echo1@gmail.com>
2024-10-06 13:40:09 -0600

    first commit

---
+++ src/main/java/com/example/interviewhippo/InterviewHippoApplication.java
@@ -0,0 +1,13 @@
+package com.example.interviewhippo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InterviewHippoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InterviewHippoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/interviewhippo/InterviewHippoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.interviewhippo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InterviewHippoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
